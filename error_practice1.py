d15552c454b0edbac2e7ef5c6a73eb54c49e9ebe
Dave Westerveld <Dave.Westerveld@D2L.com>
2019-08-22 09:37:44 -0400

    add in two error practice scripts

---
+++ error_practice1.py
@@ -0,0 +1,8 @@
+#there are 4 errors in this file.
+#run the file and see if you can fix them all
+my_list = [1,2,3]
+
+three = my_list[3]
+
+if count == True:
+my_list.length()
---
+++ error_practice2.py
@@ -0,0 +1,14 @@
+#There are 4 errors in this method
+#run the script and see if you can figure out how to fix them
+def cool_function(temperature):
+    #temperature in celcius
+    if temperature > 28:
+        print ("Turn on the air. It's "+temperature+ " degrees in here!")
+    elif temperature = 28:
+        print ("Ahh just right!")
+    else
+        print ("Brr! It's freezing in here. Someone please turn up the heat.)
+
+cool_function(25)
+cool_function(28)
+cool_function(30)
