6e9fdf16dc4303bf53e8fdd482ccf013881ddb92
Dave Westerveld <Dave.Westerveld@D2L.com>
2019-08-28 15:53:17 -0400

    document

--- errorslogged.py
+++ errorslogged.py
@@ -1,8 +1,14 @@
 def addTwoNumbersTwice(num1,num2):
+    '''This method adds two numbers together. 
+    It then adds those two numbers together again and 
+    then returns the sum of those sums.
+    
+    There is an error in the logic'''
     firstSum = num1 + num2
     secondSum = num1 + num1
     return firstSum + secondSum
 
 addedNumbers = addTwoNumbersTwice(1,2)
 
+#should equal 6
 print (addedNumbers)
