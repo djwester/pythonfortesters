ae0860f4c09455f696925851eb3824551c86ca02
Dave Westerveld <Dave.Westerveld@D2L.com>
2019-08-28 15:51:39 -0400

    give it an error

--- errorslogged.py
+++ errorslogged.py
@@ -1,6 +1,6 @@
 def addTwoNumbersTwice(num1,num2):
     firstSum = num1 + num2
-    secondSum = num1 + num2
+    secondSum = num1 + num1
     return firstSum + secondSum
 
 addedNumbers = addTwoNumbersTwice(1,2)
