74e6190a9bfcf9539608e230ba9e0b52e8140d8e
Dave Westerveld <Dave.Westerveld@D2L.com>
2019-11-29 12:13:04 -0500

    include resources

--- ConceptsPractice/Variables.py
+++ ConceptsPractice/Variables.py
@@ -7,3 +7,9 @@
 #create a variable that contains the float 1.5 and one that 
 #contains the float 2.5. Create a third variable that contains
 #the addition of both variables
+
+
+#Further learning resources:
+#https://www.w3schools.com/python/python_variables.asp
+#https://realpython.com/python-variables/
+
