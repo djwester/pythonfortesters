41836f9ed5b8a767a9ea45aef4953f6430d5b5f1
Dave Westerveld <Dave.Westerveld@D2L.com>
2020-01-30 13:18:39 -0500

    update log search file

--- log_file_search.py
+++ log_file_search.py
@@ -1,11 +1,20 @@
 with open('logfile.txt','r') as file_handle:
     log_file_list = file_handle.readlines()
 
-def get_referrer_from_iis_line(iis_line):
+def get_referrer_from_iis_line(iis_line,index):
     '''finds the referrer in the given iis line'''
-    return 
+    split_line = iis_line.split(' ')
+    return split_line[index]
+
+def get_header_index(iis_header_line):
+    split_header = iis_header_line.split(' ')
+    return split_header.index('cs(Referer)')
 
 referrer_list = []
 for line in log_file_list:
+    if line.startswith('#Fields:'):
+        header_index = get_header_index(line)
+        header_index -= 1
     if 'Chrome' in line:
-        referrer_list.append(get_referrer_from_iis_line(line))
+        referrer_list.append(get_referrer_from_iis_line(line,header_index))
+print(referrer_list)
